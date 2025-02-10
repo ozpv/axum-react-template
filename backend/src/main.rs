493f337e1f24a90bece2ad4afd4e0bde07f5ea38
ozpv <39195175+ozpv@users.noreply.github.com>
2025-02-10 13:57:27 -0600

    fix react router

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,15 @@
 use axum::{
-    handler::HandlerWithoutStateExt,
-    http::StatusCode,
-    response::{Html, IntoResponse},
-    routing::get,
-    Router,
+    extract::{Request, State}, handler::HandlerWithoutStateExt, http::StatusCode, response::{Html, IntoResponse}, routing::get, Router
 };
 use std::path::PathBuf;
 use std::time::Duration;
 use tokio::net::TcpListener;
+use tower::util::ServiceExt;
 use tower_http::{
-    compression::CompressionLayer, services::ServeDir, timeout::TimeoutLayer, trace::TraceLayer,
+    compression::CompressionLayer, services::{ServeDir, ServeFile}, timeout::TimeoutLayer, trace::TraceLayer,
 };
+
+use backend::state::AppState;
 
 struct Env {
     site_addr: String,
@@ -35,6 +34,17 @@ async fn not_found() -> impl IntoResponse {
     (StatusCode::NOT_FOUND, Html("<h1>404 Not Found</h1>"))
 }
 
+// serves static files with GET /assets/*
+async fn static_file_server(State(state): State<AppState>, req: Request) -> impl IntoResponse {
+    ServeDir::new(state.get_dist_dir().as_ref())
+        .precompressed_br()
+        .precompressed_gzip()
+        .fallback(not_found.into_service())
+        .oneshot(req)
+        .await
+        .map_err(|_| (StatusCode::NOT_FOUND, Html("<h1>404 Not Found</h1>")))
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt()
@@ -46,19 +56,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         dist_dir,
     } = Env::get_or_default();
 
-    let router = Router::new()
-        // test api
-        .route("/api/test", get(|| async { "hello from axum" }))
-        // serve the frontend statically
-        .fallback_service(ServeDir::new(&dist_dir).not_found_service(not_found.into_service()))
-        .layer(CompressionLayer::new().gzip(true))
-        .layer(TimeoutLayer::new(Duration::from_secs(30)))
-        .layer(TraceLayer::new_for_http());
+    let index_path = dist_dir.join("index.html");
 
     let listener = TcpListener::bind(&site_addr).await?;
 
     tracing::info!("Listening on http://{site_addr}/");
     tracing::info!("Serving files in {}", dist_dir.display());
+
+    let router = Router::new()
+        // test api
+        .route("/healthcheck", get(|| async { "routing works" }))
+        // serve the frontend statically
+        // serving the index file allows for react router to resume the routing
+        .fallback_service(ServeFile::new(index_path))
+        .route("/assets/{*any}", get(static_file_server))
+        .layer(CompressionLayer::new().gzip(true))
+        .layer(TimeoutLayer::new(Duration::from_secs(30)))
+        .layer(TraceLayer::new_for_http())
+        .with_state(AppState::new(dist_dir));
+
     axum::serve(listener, router).await?;
     Ok(())
 }
